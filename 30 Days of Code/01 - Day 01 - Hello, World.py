9dd9d7301c11626ba0769439365950fa514ee128
Pranv <sonawanepranav19@gmail.com>
2021-11-07 16:40:37 +0530

    Day 01

---
+++ 30 Days of Code/01 - Day 01 - Hello, World.py
@@ -0,0 +1,21 @@
+
+# ========================
+#       Information
+# ========================
+
+# Direct Link: https://www.hackerrank.com/challenges/30-hello-world/problem
+# Difficulty: Easy
+# Max Score: 30
+# Language: Python
+
+# ========================
+#         Solution
+# ========================
+
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+print(input_string)
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
